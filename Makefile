4da960c9aa042a2a3668b5f0402f578e7e6d58a2
Sergei Gannochenko <s.gannochenko@c02c180mmd6t.ad.deliveryhero.com>
2023-06-19 19:04:20 +0200

    wip

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 run_infra: ## Run the infrastructure locally
-	@docker-compose up
+	@docker-compose up --build
--- go-app/cmd/main.go
+++ go-app/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
@@ -10,10 +11,22 @@ import (
 )
 
 var (
+	// RequestCount is a sample counter metrics
 	RequestCount = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "go_app_request_count",
 		Help: "Total APP HTTP Request count",
 	})
+	// RequestInProgress is a sample gauge metrics
+	RequestInProgress = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "go_app_request_in_progress",
+		Help: "HTTP Request in progress",
+	})
+	// RequestResponseTime is a sample summary metrics
+	// promql average latency: rate(go_app_response_latency_sum[5m]) / rate(go_app_response_latency_count[5m])
+	RequestResponseTime = promauto.NewSummaryVec(prometheus.SummaryOpts{
+		Name: "go_app_response_latency",
+		Help: "Response latency",
+	}, []string{"path"})
 )
 
 func prometheusHandler() gin.HandlerFunc {
@@ -24,13 +37,28 @@ func prometheusHandler() gin.HandlerFunc {
 	}
 }
 
+func latencySampler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		startTime := time.Now()
+
+		c.Next()
+
+		RequestResponseTime.WithLabelValues(c.Request.RequestURI).Observe(time.Since(startTime).Seconds())
+	}
+}
+
 // https://stackoverflow.com/questions/65608610/how-to-use-gin-as-a-server-to-write-prometheus-exporter-metrics
 func main() {
 	r := gin.Default()
+	r.Use(latencySampler())
 
 	r.GET("/ping", func(c *gin.Context) {
+		RequestInProgress.Inc()
+
+		time.Sleep(2 * time.Second)
 		RequestCount.Inc()
 
+		RequestInProgress.Dec()
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
